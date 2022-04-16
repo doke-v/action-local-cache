759e2a91126ed0f28c96f2f44fa4b66aab174b2d
tuhnik <huntlarsen@gmail.com>
2022-04-16 16:39:35 +0300

    init

---
+++ src/post.ts
@@ -0,0 +1,20 @@
+import { setFailed } from '@actions/core'
+import { mkdirP, cp } from '@actions/io'
+
+import { getVars } from './lib/getVars'
+import { isErrorLike } from './lib/isErrorLike'
+import log from './lib/log'
+
+async function post(): Promise<void> {
+  try {
+    const { cacheDir, targetPath, cachePath } = getVars()
+    await mkdirP(cacheDir)
+    await cp(targetPath, cachePath, { recursive: true })
+    log.info('copied from', targetPath, 'to', cachePath)
+  } catch (error: unknown) {
+    log.trace(error)
+    setFailed(isErrorLike(error) ? error.message : `unknown error: ${error}`)
+  }
+}
+
+void post()
